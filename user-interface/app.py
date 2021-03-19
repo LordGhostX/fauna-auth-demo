68bde514e48ae5b424b763ad00950e0d31117302
LordGhostX <solomonghost9@gmail.com>
2021-03-19 14:23:09 +0100

    changed demo structure

---
+++ user-interface/app.py
@@ -0,0 +1,34 @@
+from flask import *
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/dashboard/")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/dashboard/logout/<string:logout_type>/")
+def logout(logout_type):
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
