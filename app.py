915fcca83c3947792d957c84c0fad4758267fee4
LordGhostX <solomonghost9@gmail.com>
2021-03-19 04:18:52 +0100

    added register route

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
