b5ac924a7203443bd9372069abc55357f637ebc3
LordGhostX <solomonghost9@gmail.com>
2021-03-19 09:38:04 +0100

    added logout route

--- app.py
+++ app.py
@@ -25,5 +25,10 @@ def dashboard():
     return render_template("dashboard.html")
 
 
+@app.route("/dashboard/logout/<string:logout_type>/")
+def logout(logout_type):
+    return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
